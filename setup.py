0bbe3d6e8256e718ddada49bc0621621c8f21eb9
Paul Yuan <paul.yuan@appier.com>
2017-09-14 10:21:10 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='nanoid',
+      version='0.1',
+      description='Python nanoid implementation',
+      url='https://github.com/puyuan/py-nanoid',
+      author='Paul Yuan',
+      author_email='puyuan1@gmail.com',
+      license='MIT',
+      py_modules=['nanoid'],
+      zip_safe=False)
