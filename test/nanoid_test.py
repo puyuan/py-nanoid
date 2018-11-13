8d05b09bf759c59ea8b1922f5e86c1fc72c4f253
aidarkhanov <36963426+aidarkhanov@users.noreply.github.com>
2018-11-13 09:42:56 +0600

    fix: moved from pytest to standard unittest

--- test/nanoid_test.py
+++ test/nanoid_test.py
@@ -1,48 +1,55 @@
 # coding: utf-8
 
+from sys import maxsize
+from unittest import TestCase
+
 from nanoid import generate
-from pytest import approx
 
 alphabet = '_-0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 
-def test_generates_url_friendly_id():
-    for _ in range(10):
-        id = generate()
-        assert len(id) == 21
-        for j in range(len(id)):
-            assert id[j] in alphabet
+class TestNanoID(TestCase):
+    def test_generates_url_friendly_id(self):
+        for _ in range(10):
+            id = generate()
+            self.assertEqual(len(id), 21)
+            for j in range(len(id)):
+                self.assertIn(id[j], alphabet)
 
+    def test_flat_distribution(self):
+        count = 100 * 1000
+        length = 5
+        alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-def test_flat_distribution():
-    count = 100 * 1000
-    length = 5
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+        chars = {}
+        for _ in range(count):
+            id = generate(alphabet, length)
+            for j in range(len(id)):
+                char = id[j]
+                if not chars.get(char):
+                    chars[char] = 0
+                chars[char] += 1
 
-    chars = {}
-    for _ in range(count):
-        id = generate(alphabet, length)
-        for j in range(len(id)):
-            char = id[j]
-            if chars.get(char) == None:
-                chars[char] = 0
-            chars[char] += 1
+        self.assertEqual(len(chars.keys()), len(alphabet))
 
-    assert len(chars.keys()) == len(alphabet)
+        max = 0
+        min = maxsize
+        print(chars)
+        for k in chars:
+            distribution = (chars[k] * len(alphabet)) / float((count * length))
+            if distribution > max:
+                max = distribution
+            if distribution < min:
+                min = distribution
+        self.assertLessEqual(max - min, 0.05)
 
-    for k in chars:
-        distribution = (chars[k] * len(alphabet)) / (count * length)
-        assert distribution == approx(1, 1)
+    def test_has_no_collisions(self):
+        count = 100 * 1000
+        used = {}
+        for _ in range(count):
+            id = generate()
+            self.assertIsNotNone(used.has_key(id))
+            used[id] = True
 
-
-def test_has_no_collisions():
-    count = 100 * 1000
-    used = {}
-    for _ in range(count):
-        id = generate()
-        assert not used.has_key(id)
-        used[id] = True
-
-
-def test_has_options():
-    assert generate('a', 5) == 'aaaaa'
+    def test_has_options(self):
+        self.assertEqual(generate('a', 5), 'aaaaa')
