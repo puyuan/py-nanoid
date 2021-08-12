040341eaf944dc2ccdbf8874d07717f7871d436e
Bob Ippolito <bob@redivi.com>
2021-08-12 15:54:48 +0000

    Improve mask computation and simplify method
    `mask >= alphabet_len - 1` must be True, but the difference
    should be minimized. For powers of 2 we expect
    `mask == alphabet_len - 1 `.

--- nanoid/method.py
+++ nanoid/method.py
@@ -9,18 +9,14 @@ def method(algorithm, alphabet, size):
 
     mask = 1
     if alphabet_len > 1:
-        mask = (2 << int(log(alphabet_len - 1) / log(2))) - 1
+        mask = (1 << int(ceil(log(alphabet_len) / log(2)))) - 1
     step = int(ceil(1.6 * mask * size / alphabet_len))
 
     id = ''
     while True:
-        random_bytes = algorithm(step)
-
-        for i in range(step):
-            random_byte = random_bytes[i] & mask
+        for random_byte in algorithm(step):
+            random_byte = random_byte & mask
             if random_byte < alphabet_len:
-                if alphabet[random_byte]:
-                    id += alphabet[random_byte]
-
-                    if len(id) == size:
-                        return id
+                id += alphabet[random_byte]
+                if len(id) == size:
+                    return id
